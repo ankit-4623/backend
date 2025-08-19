087ceff24fff6072bf377cbcebdfa4dabdc95924
ankit-4623 <ankitnayek007@gmail.com>
2025-08-20 01:14:16 +0530

    update server.js

--- server.js
+++ server.js
@@ -68,10 +68,10 @@ app.use((req, res, next) => {
 
 // Database Configurations
 const banerjeeConfig = {
-  host: "82.25.106.64",
-  user: "u617065149_Banerjee",
-  password: "SwattikA1",
-  database: "u617065149_Banerjee_Elect",
+  host: process.env.BN_DB_HOST,
+  user: process.env.BN_DB_USER,
+  password: process.env.BN_DB_PASS,
+  database: process.env.BN_DB_NAME,
    waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
